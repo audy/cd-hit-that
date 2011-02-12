792696fd301ee34ef86df5eda81a75ec833edf6d
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-02-11 21:24:25 -0500

    test

--- go.sh
+++ go.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
 CUTOFF=10
-reads=$1
-out=$2
+p=$1
 
-echo $reads $out $CUTOFF
+echo $p
 
 init () {
-  mkdir -p $out/clusters
-  mkdir -p $out/joined
-  mkdir -p $out/reprs
+  mkdir -p $p/clusters
+  mkdir -p $p/joined
+  mkdir -p $p/reprs
 }
 
 label_join () {
   # Label and join reads
-    for file in reads/*.txt
+    for file in $p/reads/*.txt
     do
       echo "Labelling and joining $file"
       mkdir -p joined/$i
-      cat $file | python labels.py > $out/joined/$(basename $file).fa
+      cat $file | python labels.py > $p/joined/$(basename $file).fa
     done
 }
 
 cluster () {
   # Cluster joined reads with CDHIT
-    for file in $out/joined/*.txt
+    for file in $p/joined/*.txt
     do
       echo "Clustering $file"
-      sh run_cdhit.sh $file $out/clusters/$(basename $file)
+      sh run_cdhit.sh $file $p/clusters/$(basename $file)
     done
 }
 
 filter () {
   # Filter out rep. reads that belong to clusters only consting of themself
-  for file in $out/clusters/*.txt
+  for file in $p/clusters/*.txt
   do
     echo "Filtering singletons from $file, cutoff = $CUTOFF"
-    python filter.py $file $file.clstr $CUTOFF > $out/reprs/$(basename $file)
+    python filter.py $file $file.clstr $CUTOFF > $p/reprs/$(basename $file)
   done
 }
+
 init
 
 label_join
