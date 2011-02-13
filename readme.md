b1bd78a48277d17c2ea7665c25918eb8b0647c8a
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-02-13 17:56:14 -0500

    ..

--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
 # CDHIT Thing
+
+To run, do this:
+
+    ./run <cutoff>
+    
+Where cutoff is the minimum number of reads required for a cluster to be reported.
+
+A tab-separated sheet containing cluster, group (negative or positive), and reads for that cluster and group will be printed to STDOUT.
+
+A fasta file `reps.fasta` containing representative reads will be generated.
+
--- src/filter.py
+++ src/filter.py
@@ -1,7 +1,7 @@
 import sys
 from collections import defaultdict
 
-clust_file = sys.argv[2]
+clust_file = sys.argv[1]
 fasta_file = sys.argv[2]
 cutoff = int(sys.argv[3])
 
@@ -17,14 +17,31 @@ with open(clust_file) as handle:
             line = line.split()
             group = line[2][1:].rstrip('.').split(':')[0]
             counts[cluster][group] += 1
-            read_to_clust[line[2].rstrip('.')] = cluster
+            read_to_clust[line[2].rstrip('.')[1:].split(':')[1]] = cluster
             
-# Print out representatives
 
+# Print counts
+for cluster in counts:
+    if not ((counts[cluster]['N'] > cutoff) and (counts[cluster]['P'] > cutoff)):
+        continue
+    for group in ('N', 'P'):
+        if group in counts[cluster]:
+            print >> sys.stderr, "%s\t%s\t%s" % (group, cluster.split()[1], counts[cluster][group])
+        else:
+            print >> sys.stderr, "%s\t%s\t%s" % (group, cluster.split()[1], 0)
+        
+
+# Print out representatives
 with open(fasta_file) as handle:
     for line in handle:
         if line.startswith('>'):
+            keep = False
             group, read = line.strip().split(':')
             cluster = read_to_clust[read]
-            count = counts[cluster][group]
-            print >> sys.stderr, "%s\t%s" % (cluster, count)
+            if not ((counts[cluster]['N'] > cutoff) and (counts[cluster]['P'] > cutoff)):
+                continue
+            else:
+                keep = True
+                print '>%s Group_%s' % (cluster, group[1:])
+        elif keep:
+            print line.strip()
